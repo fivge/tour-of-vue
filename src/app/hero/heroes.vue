a332c98d80e9bdc333e4c708864b3b51ac253ce9
fivge <luanxingtong@gmail.com>
2020-07-04 17:01:05 +0800

    toh-pt2

--- src/app/hero/heroes.vue
+++ src/app/hero/heroes.vue
@@ -1,37 +1,102 @@
 <template>
   <div>
-    <!-- <h2>{{hero.name | uppercase}} Details</h2> TODO: 管道-->
-    <h2>{{ heroName }} Details</h2>
-    <div>
-      <span>id:</span>
-      {{ hero.id }}
-    </div>
-    <div>
-      <label>
-        name:
-        <input v-model="hero.name" placeholder="name" />
-      </label>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li
+        v-for="hero of heroes"
+        :key="hero.id"
+        :class="{ selected: hero === selectedHero }"
+        @click="onSelect(hero)"
+      >
+        <span class="badge">{{hero.id}}</span>
+        {{hero.name}}
+      </li>
+    </ul>
+
+    <div v-if="selectedHero">
+      <!-- <h2>{{selectedHero.name | uppercase}} Details</h2> TODO: 管道-->
+      <h2>{{ selectedHeroName }} Details</h2>
+      <div>
+        <span>id:</span>
+        {{ selectedHero.id }}
+      </div>
+      <div>
+        <label>
+          name:
+          <input v-model="selectedHero.name" placeholder="name" />
+        </label>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { HEROES } from './mock-heroes';
+
 export default {
   name: 'Heroes',
   data: function() {
     return {
-      hero: {
-        id: 1,
-        name: 'Windstorm'
-      }
+      heroes: HEROES,
+      selectedHero: null
     };
   },
   computed: {
-    heroName: function() {
-      return `${this.hero.name}`.toUpperCase();
+    selectedHeroName: function() {
+      return `${this.selectedHero.name}`.toUpperCase();
+    }
+  },
+  methods: {
+    onSelect(hero) {
+      this.selectedHero = hero;
     }
   }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+/* HeroesComponent's private CSS styles */
+.heroes {
+  margin: 0 0 2em 0;
+  list-style-type: none;
+  padding: 0;
+  width: 15em;
+}
+.heroes li {
+  cursor: pointer;
+  position: relative;
+  left: 0;
+  background-color: #eee;
+  margin: 0.5em;
+  padding: 0.3em 0;
+  height: 1.6em;
+  border-radius: 4px;
+}
+.heroes li:hover {
+  color: #607d8b;
+  background-color: #ddd;
+  left: 0.1em;
+}
+.heroes li.selected {
+  background-color: #cfd8dc;
+  color: white;
+}
+.heroes li.selected:hover {
+  background-color: #bbd8dc;
+  color: white;
+}
+.heroes .badge {
+  display: inline-block;
+  font-size: small;
+  color: white;
+  padding: 0.8em 0.7em 0 0.7em;
+  background-color: #405061;
+  line-height: 1em;
+  position: relative;
+  left: -1px;
+  top: -4px;
+  height: 1.8em;
+  margin-right: 0.8em;
+  border-radius: 4px 0 0 4px;
+}
+</style>
